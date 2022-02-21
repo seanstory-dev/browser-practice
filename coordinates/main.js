f6eff05e69cf7750384f71c34ecf753db9741496
seanstory-dev <seanstory.dev@gmail.com>
2022-02-21 10:47:33 +0900

    Refactor coordinates project

--- coordinates/main.js
+++ coordinates/main.js
@@ -1,20 +1,34 @@
-const horizontal = document.querySelector('.horizontal');
-const vertical = document.querySelector('.vertical');
-const target = document.querySelector('.target');
-const tag = document.querySelector('.tag');
+const horizontal = document.querySelector(".horizontal");
+const vertical = document.querySelector(".vertical");
+const target = document.querySelector(".target");
+const tag = document.querySelector(".tag");
 
-document.addEventListener('mousemove', (e) => {
-  console.log(e.clientX, e.clientY);
-  const x = e.clientX;
-  const y = e.clientY;
+addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetWidth = targetRect.width / 2;
+  const targetHeight = targetRect.height / 2;
 
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
+  document.addEventListener("mousemove", (e) => {
+    // console.log(e.clientX, e.clientY);
+    const x = e.clientX;
+    const y = e.clientY;
 
-  target.style.top = `${y}px`;
-  target.style.left = `${x}px`;
+    // vertical.style.left = `${x}px`;
+    // horizontal.style.top = `${y}px`;
 
-  tag.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.innerHTML = `${x}px ${y}px`;
+    // target.style.top = `${y}px`;
+    // target.style.left = `${x}px`;
+
+    // tag.style.top = `${y}px`;
+    // tag.style.left = `${x}px`;
+
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetWidth}px, ${
+      y - targetHeight
+    }px)`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+
+    tag.innerHTML = `${x}px ${y}px`;
+  });
 });
